2d46b90b719bad223cb2078a79d685df7a6f3602
Mateusz <316072@uwr.edu.pl>
2022-11-03 21:46:54 +0100

    Init template

---
+++ src/Basic.cpp
@@ -0,0 +1,6 @@
+#include "../include/Basic.hpp"
+
+std::string Lib_SayHello()
+{
+  return "Hello from the library!";
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "../include/Basic.hpp"
+
+#include <iostream>
+
+int main()
+{
+  std::cout << Lib_SayHello() << "\n";
+  return 0;
+}
